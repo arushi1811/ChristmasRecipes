d317bd1cf37a844ffa3ae8a2070d40f5114cca34
arushi1811 <arushig@bu.edu>
2022-01-26 17:30:43 -0600

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 
-class Bagpack {
-    
-}
-
+// Big array of 
+// {
+    //     Name: 'Christmas slaw',
+    //     url: 'https://www.bbcgoodfood.com/recipes/890635/christmas-slaw',
+    //     Description: 'A nutty winter salad which is superhealthy, quick to prepare and finished with a light maple syrup dressing',
+    //     Author: 'Good Food',
+    //     Ingredients: [
+        //       '½ white cabbage, shredded',
+        //       '2 red peppers, deseeded and sliced',
+        //       '2 tbsp maple syrup',
+        //       '2 tsp Dijon mustard',
+        //       '8 tbsp olive oil',
+        //       '4 tbsp cider vinegar'
+        //     ],
+        //     Method: [
+            //       'Peel strips from the carrots using a vegetable peeler, then mix with the other salad ingredients in a large bowl. Combine all the dressing ingredients in a jam 
+            //   jar, season, then put the lid on and shake well. Toss through the salad when you’re ready to eat it. The salad and dressing will keep separately in the fridge for up to four days.'
+            //     ]
+            
 let recipe = require('./recipes')
 console.log(recipe.length);
 console.log(recipe[5]);
